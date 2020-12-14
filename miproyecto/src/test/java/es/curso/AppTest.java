69b61670af68ac308378ebf0d9e77d746fb43af7
Ubuntu <ubuntu@ip-172-31-9-6.eu-west-1.compute.internal>
2020-12-14 18:41:02 +0000

    pruebas

--- miproyecto/src/test/java/es/curso/AppTest.java
+++ miproyecto/src/test/java/es/curso/AppTest.java
@@ -16,5 +16,56 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+        /*
+            Hay 3 posibles estados de salida de una prueba unitaria:
+                - EXITO
+                - FALLO, la prueba no ha salido como se esperaba
+                - EXPLOSION
+            Se basan en el concepto de ASEVERACION: ASSERT
+            Quiero asegurar que se cumple algo... 
+                Si ese algo se cumple, doy la prueba por satisfecha
+        */
     }
+
+    
+    @Test
+    public void pruebaDoblar(){
+        int resultado=App.doblar(5);
+        assertEquals( resultado, 10 );
+    }
+
+    @Test
+    public void pruebaDoblarCero(){
+        int resultado=App.doblar(0);
+        assertEquals( resultado, 0 );
+    }
+    @Test
+    public void pruebaDoblarNegativo(){
+        int resultado=App.doblar(-5);
+        assertEquals( resultado, -10 );
+    }
+
+    @Test
+    public void pruebaMayorPrimero(){
+        int resultado=App.mayor(10, 5);
+        assertEquals( resultado, 10 );
+    }
+
+    @Test
+    public void pruebaMayorSegundo(){
+        int resultado=App.mayor(5,10);
+        assertEquals( resultado, 10 );
+    }
+    @Test
+    public void pruebaMayorIguales(){
+        int resultado=App.mayor(5,5);
+        assertEquals( resultado, 5 );
+    }
+    @Test
+    public void pruebaMayorNegativos(){
+        int resultado=App.mayor(5,-10);
+        assertEquals( resultado, 5 );
+    }
+
+    
 }
